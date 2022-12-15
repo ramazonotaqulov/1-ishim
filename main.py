657d662b846fde49ac32857ede82e7e9b1e2a028
ramazon <ramazonbek710@gmail.com>
2022-12-15 13:39:26 +0500

    new cod

---
+++ main.py
@@ -0,0 +1,51 @@
+print('1-vazifa')
+
+sonlar = [1,2,3,4,5,6,7,8,9,10]
+print(f' natija :{sum(sonlar)}')
+
+print('2-vazifa, bo\'sh')
+
+
+
+
+
+print('3-vazifa')
+
+raqamlar = [13, 5, 3, 7, 15, 25]
+kvadrati = [ ]
+for x in raqamlar:
+    kvadrati.append(x**2)
+print(kvadrati)
+
+print("4-vazifa")
+
+aralash = [32, -12, 11, 42, -6, -65, 19]
+musbat = [ ]
+manfiy = [ ]
+for x in aralash:
+    if x>0:
+        musbat.append(x)
+    else:
+        manfiy.append(x)
+print(f"manfiy sonlar:{manfiy}")
+print(f"musbat sonlar{musbat}")
+
+print('5-vazifa')
+
+aralash1 = [32, 29, 11, 42, 66, 123, 87, 90, 45]
+
+juftson=[ ]
+toqson =  [ ]
+for x in aralash1:
+    if x%2==0:
+        juftson.append(x)
+    else:
+        toqson.append(x)
+print(f'juft sonlar :{juftson}')
+print(f'toq sonlar :{toqson}')
+
+print('6-vazifa b\'sh')
+
+
+
+
